818c93190d65117b9c79101fac7e36757dec2832
meyers007 <sada.narayanappa@gmail.com>
2023-09-02 01:32:20 -0400

    Commiting

--- apps/settings.py
+++ apps/settings.py
@@ -1,4 +1,4 @@
-DEFAULT_APP = "example_app"
+DEFAULT_APP = "edu"
 
 '''
 You can include this in your html pages and refer to these variables:
@@ -10,9 +10,9 @@ For example:
 #---------------------------------------------------------------------------------
 def appcontext(request):
     context = {
-        "appname": "Example Application",
+        "appname": "AI University",
         "weburl" : "https://www.geospaces.org/",
-        "top_url": "example_app/topbar.html"
+        "top_url": "edu/topbar.html"
     }
     
     return context

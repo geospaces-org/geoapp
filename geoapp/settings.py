5ddb382c0cd9385b30a300e3229405a011082920
meyers007 <sada.narayanappa@gmail.com>
2023-09-03 05:50:31 -0400

    Commiting

--- geoapp/settings.py
+++ geoapp/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'pr75j7t*r!j=oac!798tazlecdo0%k0rasre@!f_&0u%2(=nty'
 
-import os, sys, glob
+import os, sys, glob 
 from django.urls import path, include
 
 sys.path.append(os.path.expanduser("~/.django") )

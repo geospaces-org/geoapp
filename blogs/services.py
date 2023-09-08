74178cd15df6f6b0d622bd19b5d89741206a0eb6
meyers007 <sada.narayanappa@gmail.com>
2023-09-08 03:48:52 -0400

    Commiting

--- blogs/services.py
+++ blogs/services.py
@@ -2,51 +2,83 @@
 
 #*** DO NOT EDIT - GENERATED FROM services.ipynb ****
 
-import os, json, sys
+import os, json, sys, datetime, geoapp, aiservices, logging
 from  mangorest.mango import webapi
-from geoapp.utils import read_csv, readfile
-
 import aiservices
 
-BASE = "/opt/data/data/articles/"
+BASE = "/opt/data/data/articles/raw/"
+logger = logging.getLogger( "app.blogs")
 
+#--------------------------------------------------------------------------------------------------------
+DIRS = ("blogs/static/blogs/data/", "static/docs/", BASE)
+
+def find(file, dirs=DIRS):
+    if os.path.exists(file):
+        return file
+    for d in dirs:
+        f = os.path.join(d, file)
+        if os.path.exists(f):
+            return f
+    return None
+#--------------------------------------------------------------------------------------------------------
+@webapi("/blogs/getarticle")
+def getarticle(request, file="", viewid="", dirs=DIRS, **kwargs):
+    file = file or viewid
+    f = find(file)
+    if (not f):
+        return f'{file} - does not exist!!!';
+
+    cont = geoapp.utils.readfile(f )
+    meta = json.loads(geoapp.utils.readfile(f+".meta", "{}"))
+    title= meta.get('title', '')
+
+    ret = { "title": title, "content": cont , "meta": meta, "dir": os.path.dirname(f) }
+    return json.dumps(ret)
 #--------------------------------------------------------------------------------------------------------    
-DEFAULT_FILE = f'{BASE}/stocks.csv'
-@webapi("/stocks/getsymbols")
-def getsymbols(request, user="", **kwargs):
-    file = f'{BASE}/{user}/stocks.csv'
-    if (not user or not os.path.exists(file)):
-        file = DEFAULT_FILE
-
-    df, ret = read_csv(file)
-    return ret 
+@webapi("/blogs/createarticle")
+def article(request, user="noname", cfilename="", title="", contents="", **kwargs):
+    if (not cfilename):
+        cfilename = f"{user}-{datetime.datetime.now().strftime('%Y-%m-%dT%H-%M-%S')}.md"
 #--------------------------------------------------------------------------------------------------------    
-@webapi("/stocks/getDetails")
-def getdetails(request, q="dow_jones", duration="all", user="", **kwargs):
-    symbol = q
-    csv     = f'{BASE}/{symbol}/daily.csv'
-    csv_aug = f'{BASE}/{symbol}/daily_aug.csv'
+@webapi("/blogs/savearticle")
+def savearticle(request, user="noname", cfilename="", title="", contents="", **kwargs):
+    if (not contents):
+        return "No contents!!"
 
-    if os.path.exists(csv_aug):
-        csv = csv_aug
-    elif ( not os.path.exists(csv  )):
-        csv   = f'{BASE}/dow_jones/daily.csv'
+    file = find(cfilename)
+    if (not file):
+        if (not cfilename):
+            file = f"{user}-{datetime.datetime.utcnow().replace(microsecond=0).isoformat()}.md"
+        else:
+            file = DIRS[0] + cfilename
 
-    news    = f'{BASE}/{symbol}/news.html'
-    stats1  = f'{BASE}/{symbol}/summary.json'
-    stats2  = f'{BASE}/{symbol}/fundamentals.json'
+    logger.info(f"===> Saving {file} ...")
+    indexed = file+".mdd"
+    if os.path.exists(indexed):
+        os.remove(indexed)
 
-    if (not os.path.exists(news  )): news  = f'{BASE}/dow_jones/news.html'
-    if (not os.path.exists(stats1)): stats1= f'{BASE}/dow_jones/summary.json'
+    meta = file+".meta"
+    if os.path.exists(meta):
+        os.remove(meta)
 
-    s1 = json.loads(readfile(stats1, "{}"))
-    s2 = json.loads(readfile(stats2, "{}"))
-    s2.update(s1)
+    dirn = os.path.dirname(cfilename)
+    if(not os.path.exists(dirn) ): 
+        os.makedirs( dirn ) 
 
-    print(f"Symbol Details :{csv} - {news} {stats1}")
+    # Upload file attachments if any
+    files = geoapp.utils.uploadFiles(request )
+    kwargs['files']    = files
+    kwargs["filename"] = cfilename
+    kwargs["title"]    = title
+    kwargs["user"]     = user
+    kwargs["date"]     = f"{datetime.datetime.utcnow()}" 
 
-    df, ret = read_csv(csv)
-    ret['news'] = readfile(news, "")
-    ret['stats']= json.dumps(s2)
+    with open(meta, "w") as f:
+        f.write(json.dumps(kwargs, indent = 4) )
 
-    return ret
+    with open(cfilename, "wb") as f:
+        f.write(contents.encode('utf-8'))
+
+    #os.system('/opt/utils/filestoes.py -d "/opt/data/data/articles/raw" -t "*.md" &')
+        
+    return f'Saved: {json.dumps(kwargs, indent = 4)} '

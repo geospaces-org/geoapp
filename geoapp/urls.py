4f86497af60e49a15b7db02a6c9151f1dd332fa4
meyers007 <sada.narayanappa@gmail.com>
2023-09-09 22:48:55 -0500

    Commiting

--- geoapp/urls.py
+++ geoapp/urls.py
@@ -1,25 +1,23 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import views
 from mangorest import mango
-from django.conf import settings
-#from . import settings
+import apps.settings
 
 app_name = 'geoapp'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path(r'uploadfile', views.uploadfile, name='uploadfile'),
     path(r'', views.index, name='index'),
-] + settings.DETECTED_URLS + [
+] + apps.settings.INSTALLED_URLS + [
     path('oidc/', include('mozilla_django_oidc.urls')),
     re_path(r'^.*/$', mango.Common, name='catchall'),
 ]
+urlpatterns = staticfiles_urlpatterns() + urlpatterns
 
-#urlpatterns = staticfiles_urlpatterns() + urlpatterns
-urlpatterns =  urlpatterns
-#print("++ geoapp/urls.py: urlpatterns:", urlpatterns)
+
 '''
 To enable single sign on: 
 

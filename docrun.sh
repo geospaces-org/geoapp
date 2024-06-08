d4b6d58c4429cc95aeb883665b326526464d1f92
geospaces-org <geo1@geospaces.org>
2024-06-07 21:38:30 -0600

    Commiting

--- docrun.sh
+++ docrun.sh
@@ -1,1 +1,7 @@
 docker run --rm -it --name stream1 -p 8888:8888 -p 7003:7003  --network demonet geospaces/test:streamdev
+
+
+echo ** To COMMIT YOUR IMAGE ***
+	echo docker commit stream1 streamdev
+	echo docker tag streamdev geospaces/test:streamdev 
+	echo docker push geospaces/test:streamdev 

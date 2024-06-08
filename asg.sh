7b1389682ac2654b51d538de23ca42d56648b62a
geospaces-org <geo1@geospaces.org>
2024-06-07 21:47:33 -0600

    Commiting

--- asg.sh
+++ asg.sh
@@ -7,5 +7,5 @@ fi
 # uncomment the following to run in https mode
 OPTS=' --cert /tmp/cert'
 
-daphne -b 0.0.0.0 -p ${PORT}  geoapp.asgi:application 
-
+#daphne  -b 0.0.0.0       -p    ${PORT}  geoapp.asgi:application 
+uvicorn --host 0.0.0.0  --port ${PORT}  geoapp.asgi:application --reload

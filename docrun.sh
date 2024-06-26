d26285193fff54298e96bb5ef9aa080ce346ffc6
geospaces-org <geo1@geospaces.org>
2024-06-26 08:41:59 -0600

    Commiting

--- docrun.sh
+++ docrun.sh
@@ -1,3 +1,6 @@
+NW=--network demonet
+docker network ls | grep demonet ; if [ $? -ne 0 ]; then docker network create demonet ; fi
+
 docker run --rm -it --name stream1 -p 8888:8888 -p 7003:7003  --network demonet geospaces/test:streamdev
 
 

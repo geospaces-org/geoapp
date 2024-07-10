d0607d2cde22f1215018005fc78bfaa290cdf0d3
geospaces-org <geo1@geospaces.org>
2024-07-10 08:04:05 -0600

    Commiting

--- apps/settings.py
+++ apps/settings.py
@@ -1,4 +1,4 @@
-DEFAULT_APP = "example_app"
+DEFAULT_APP = "tseries"
 
 '''
 You can include this in your html pages and refer to these variables:
@@ -12,6 +12,7 @@ def appcontext(request):
     context = {
         "appname": "STREAM",
         "weburl" : "https://stream.geospaces.org/",
-        "top_url": "tseries/topbar.html"
+        "top_url": "tseries/topbar.html",
+		"SSO": 1
     }    
     return context
